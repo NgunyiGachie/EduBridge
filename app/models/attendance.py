c7ecffe924274e904e0eaceae2724ee42b3b53c9
NgunyiGachie <antogachie@gmail.com>
2024-09-10 00:44:39 +0300

    Update attendance model

---
+++ app/models/attendance.py
@@ -0,0 +1,45 @@
+from database import db
+from sqlalchemy.orm import validates
+from datetime import datetime
+
+class Attendance(db.Model):
+    __table__ = 'attendances'
+    id = db.Column(db.Integer, primary_key=True)
+    student_id = db.Column(db.Integer, db.ForeignKey("student.id", nullable=False))
+    lecture_id = db.Column(db.Integer, db.ForeignKey("lecture.id", nullable=False))
+    attendance_status = db.Column(db.String, nullable=False)
+    dates = db.Column(db.DateTime, server_default=db.func.now(), nullable=False)
+
+    students = db.relationship('Students', back_populates='attendance', lazy="dynamic", cascade="all, delete-orphan")
+    lecture = db.relationship('Lecture', back_populates='attendance', lazy="dynamic", cascade="all, delete-orphan")
+
+    @validates('dates')
+    def validate_dates(self, key, value):
+        if not isinstance(value, datetime):
+            raise AttributeError(f"{key} must be a valid datetime")
+        return value
+    
+    @validates('attendance_status')
+    def validates_attendance_status(self, key, attendance_status):
+        if not isinstance(attendance_status, str):
+            raise ValueError("Attendance status must be a string")
+        attendance_status = attendance_status.strip().lower()
+        if attendance_status is None:
+            return ValueError("Attendance status cannot be None")
+        if attendance_status not in ['present', 'absent']:
+            return ValueError('Attendance status must be either present or absent')
+        return attendance_status
+    
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'student_id': self.student_id,
+            'lecture_id': self.lecture_id,
+            'attendance_status': self.attendance_status,
+            'dates': self.dates
+        }
+    
+    def __repr__(self):
+        return f"<Attendance {self.id}>"
+        
+    
