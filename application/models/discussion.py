e9c5c34cad8950a580327df21d84d140ec5b7fc3
NgunyiGachie <antogachie@gmail.com>
2024-10-10 21:24:29 +0300

    Passing discussion model tests

--- application/models/discussion.py
+++ application/models/discussion.py
@@ -1,17 +1,34 @@
 from database import db
-from application.utils.validation_mixin import ValidationMixin
+from sqlalchemy.orm import validates
+from datetime import datetime
 
-class Discussion(db.Model, ValidationMixin):
+class Discussion(db.Model):
     __tablename__   = 'discussions'
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String, nullable=False)
     description = db.Column(db.String, nullable=False)
     course_id = db.Column(db.Integer, db.ForeignKey('courses.id'), nullable=False)
-    created_at = db.Column(db.DateTime, server_default=db.func.now(), nullable=False)
-    updated_at = db.Column(db.DateTime, server_default=db.func.now(), nullable=False)
+    created_at = db.Column(db.DateTime, nullable=False)
+    updated_at = db.Column(db.DateTime, nullable=False)
 
     course = db.relationship('Course', back_populates='discussion')
     comments = db.relationship('Comment', back_populates='discussion', cascade='all, delete-orphan')
+
+    @validates('title', 'description')
+    def validate_strings(self, key, value):
+        if value is None:
+            raise ValueError("Title and description cannot be None")
+        if not isinstance(value, str):
+            raise ValueError("Title and description must be of type string")
+        if len(value) <= 0:
+            raise ValueError("Title and description must be at least one character")
+        return value
+    
+    @validates('created_at', 'updated_at')
+    def validate_dates(self, key, value):
+        if not isinstance(value, datetime):
+            raise AttributeError(f"{key} must be a valid datetime")
+        return value
 
     def to_dict(self):
         return {
