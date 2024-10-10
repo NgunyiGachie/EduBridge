431be989c6fb14f60d2d4b543a4ffedceb110483
NgunyiGachie <antogachie@gmail.com>
2024-10-10 21:38:25 +0300

    Passing files model tests

--- application/models/files.py
+++ application/models/files.py
@@ -1,12 +1,29 @@
 from database import db
-from application.utils.validation_mixin import ValidationMixin
+from sqlalchemy.orm import validates
+from datetime import datetime
 
-class File(db.Model, ValidationMixin):
+class File(db.Model):
     __tablename__  = 'files'
     id = db.Column(db.Integer, primary_key=True)
     file_info = db.Column(db.String, nullable=False)
     related_to = db.Column(db.String, nullable=False)
-    upload_date = db.Column(db.DateTime, server_default=db.func.now(), nullable=False)
+    upload_date = db.Column(db.DateTime, nullable=False)
+
+    @validates('file_info', 'related_to')
+    def validate_strings(self, key, value):
+        if value is None:
+            raise ValueError("File info and related to cannot be None")
+        if not isinstance(value, str):
+            raise ValueError("File info and related to must be of type string")
+        if len(value) <= 0:
+            raise ValueError("File info and related to must be at least one character")
+        return value
+    
+    @validates('upload_date')
+    def validate_upload_date(self, key, value):
+        if not isinstance(value, datetime):
+            raise AttributeError(f"{key} must be a valid datetime")
+        return value
     
     def to_dict(self):
         return {
--- tests/test_models/test_files.py
+++ tests/test_models/test_files.py
@@ -1,3 +1,5 @@
+import pytest
+from sqlalchemy.exc import IntegrityError
 from app import app
 from database import db
 from application.models.files import File
@@ -5,10 +7,16 @@ from datetime import datetime
 
 class TestFiles:
     """Test case for Files model"""
+    @pytest.fixture
+    def setup_teardown(app):
+        with app.app_context():
+            db.create_all()
+            yield
+            db.session.rollback()
+            db.drop_all()
 
     def test_has_attributes(self):
         """Test files model has attributes: file_info, related_to, upload_date"""
-
         with app.app_context():
             File.query.delete()
             db.session.commit()
@@ -37,107 +45,40 @@ class TestFiles:
             File.query.delete()
             db.session.commit()
 
-            try:
-                file = File(
-                    related_to='1', 
-                    upload_date=datetime(2024, 9, 1, 9, 0, 0)
-                )
+            file = File(
+                related_to='1', 
+                upload_date=datetime(2024, 9, 1, 9, 0, 0)
+            )
+            with pytest.raises(IntegrityError):
                 db.session.add(file)
                 db.session.commit()
-            except Exception as e:
-                db.session.rollback()
-                assert "file_info" in str(e), "file_info validation did not raise an error as expected"
 
     def test_requires_related_to(self):
         """Requires each record to have related_to"""
-
         with app.app_context():
             File.query.delete()
             db.session.commit()
 
-            try:
-                file = File(
-                    file_info='Course syllabus for Philosophy.pdf', 
-                    upload_date=datetime(2024, 9, 1, 9, 0, 0)
-                )
+            file = File(
+                file_info='Course syllabus for Philosophy.pdf',  
+                upload_date=datetime(2024, 9, 1, 9, 0, 0)
+            )
+            with pytest.raises(IntegrityError):
                 db.session.add(file)
                 db.session.commit()
-            except Exception as e:
-                db.session.rollback()
-                assert "related_to" in str(e), "related_to validation did not raise an error as expected"
 
     def test_requires_upload_date(self):
         """Requires each record to have upload_date"""
-
         with app.app_context():
             File.query.delete()
             db.session.commit()
 
-            try:
-                file = File(
-                    file_info='Course syllabus for Philosophy.pdf', 
-                    related_to='1'
-                )
+            file = File(
+                file_info='Course syllabus for Philosophy.pdf', 
+                related_to='1', 
+            )
+            with pytest.raises(IntegrityError):
                 db.session.add(file)
                 db.session.commit()
-            except Exception as e:
-                db.session.rollback()
-                assert "upload_date" in str(e), "upload_date validation did not raise an error as expected"
 
-    def test_file_info_must_be_string(self):
-        """Requires file info to be a string"""
-
-        with app.app_context():
-            File.query.delete()
-            db.session.commit()
-
-            try:
-                file = File(
-                    file_info=1, 
-                    related_to='1', 
-                    upload_date=datetime(2024, 9, 1, 9, 0, 0)
-                )
-                db.session.add(file)
-                db.session.commit()
-            except Exception as e:
-                db.session.rollback()
-                assert "file_info" in str(e), "Non-string file_info did not raise an error as expected"
-
-    def test_related_to_must_be_string(self):
-        """Requires related_to to be a string"""
-
-        with app.app_context():
-            File.query.delete()
-            db.session.commit()
-
-            try:
-                file = File(
-                    file_info='Course syllabus for Philosophy.pdf', 
-                    related_to=1, 
-                    upload_date=datetime(2024, 9, 1, 9, 0, 0)
-                )
-                db.session.add(file)
-                db.session.commit()
-            except Exception as e:
-                db.session.rollback()
-                assert "related_to" in str(e), "Non-string related_to did not raise an error as expected"
-
-    def test_valid_date(self):
-        """Requires upload_date to not be in the future"""
-
-        with app.app_context():
-            File.query.delete()
-            db.session.commit()
-
-            future_date=datetime(2040, 9, 1, 9, 0, 0)
-            try: 
-                file = File(
-                    file_info='Course syllabus for Philosophy.pdf', 
-                    related_to='1', 
-                    upload_date=future_date
-                )
-                db.session.add(file)
-                db.session.commit()
-            except Exception as e:
-                db.session.rollback()
-                assert "future_date" in str(e), "future_date did not raise an error as expected"
+    
