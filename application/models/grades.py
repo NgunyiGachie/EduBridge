ca7c5a2d7c27a335adbcb3a75fd63fbda324268e
NgunyiGachie <antogachie@gmail.com>
2024-10-10 21:44:13 +0300

    Passing grades model tests

--- application/models/grades.py
+++ application/models/grades.py
@@ -1,17 +1,34 @@
 from database import db
-from application.utils.validation_mixin import ValidationMixin
+from sqlalchemy.orm import validates
+from datetime import datetime
 
-class Grade(db.Model, ValidationMixin):
+class Grade(db.Model):
     __tablename__  = 'grades'
     id = db.Column(db.Integer, primary_key=True)
     student_id = db.Column(db.Integer, db.ForeignKey('students.id'), nullable=False)
     course_id = db.Column(db.Integer, db.ForeignKey('courses.id'), nullable=False)
     grade = db.Column(db.Integer, nullable=False)
-    date_posted = db.Column(db.DateTime, server_default=db.func.now(), nullable=False)
+    date_posted = db.Column(db.DateTime, nullable=False)
 
     student = db.relationship('Student', back_populates='grade')
     course = db.relationship('Course', back_populates='grade')
     submission = db.relationship('Submission', back_populates='grade', cascade='all, delete-orphan')
+
+    @validates('grade')
+    def validate_grade(self, key, grade):
+        if grade is None:
+            raise AssertionError('Grades cannot be None')
+        if not isinstance(grade, int):
+            raise AssertionError("Grades must be an integer")
+        if grade < 1 or grade > 100:
+            raise ValueError("Grades must be between 1 and 100")
+        return grade
+    
+    @validates('date_posted')
+    def validate_dates(self, key, value):
+        if not isinstance(value, datetime):
+            raise AttributeError(f"{key} must be a valid datetime")
+        return value
 
     def to_dict(self):
         return {
--- tests/test_models/test_grades.py
+++ tests/test_models/test_grades.py
@@ -1,3 +1,5 @@
+import pytest
+from sqlalchemy.exc import IntegrityError
 from app import app
 from database import db
 from application.models.grades import Grade
@@ -5,10 +7,16 @@ from datetime import datetime
 
 class TestGrades:
     """Test case for the grades model"""
+    @pytest.fixture
+    def setup_teardown(app):
+        with app.app_context():
+            db.create_all()
+            yield
+            db.session.rollback()
+            db.drop_all()
 
     def test_has_attributes(self):
         """Requires the grades model to have attributes: student_id, course_id, grades, date_posted"""
-
         with app.app_context():
             Grade.query.delete()
             db.session.commit
@@ -28,166 +36,67 @@ class TestGrades:
             assert new_grade.course_id == 2
             assert new_grade.grade == 20
             assert new_grade.date_posted == datetime(2024, 9, 1, 10, 0, 0)
-
             db.session.delete(new_grade)
             db.session.commit()
 
     def test_require_student_id(self):
         """Requires each record to have student_id"""
-
         with app.app_context():
             Grade.query.delete()
             db.session.commit()
 
-            try:
-                grade = Grade(
-                    course_id=2, 
-                    grade=20, 
-                    date_posted=datetime(2024, 9, 1, 10, 0, 0)
-                )
+            grade = Grade(
+                course_id=2, 
+                grade=20, 
+                date_posted=datetime(2024, 9, 1, 10, 0, 0)
+            )
+            with pytest.raises(IntegrityError):
                 db.session.add(grade)
                 db.session.commit()
-            except Exception as e:
-                db.session.rollback()
-                assert "student_id" in str(e), "student_id validation did not raise an error as expected"
 
     def test_requires_course_id(self):
         """Requires each record to have a course_id"""
-
         with app.app_context():
             Grade.query.delete()
             db.session.commit()
 
-            try:
-                grade = Grade(
-                    student_id=1,  
-                    grade=20, 
-                    date_posted=datetime(2024, 9, 1, 10, 0, 0)
-                )
+            grade = Grade(
+                student_id=1, 
+                grade=20, 
+                date_posted=datetime(2024, 9, 1, 10, 0, 0)
+            )
+            with pytest.raises(IntegrityError):
                 db.session.add(grade)
                 db.session.commit()
-            except Exception as e:
-                db.session.rollback()
-                assert "course_id" in str(e), "course_id validation did not raise an error as expected"
 
     def test_requires_grades(self):
         """Requirs each record to have grades"""
-
         with app.app_context():
             Grade.query.delete()
             db.session.commit()
 
-            try:
-                grade = Grade(
-                    student_id=1, 
-                    course_id=2, 
-                    date_posted=datetime(2024, 9, 1, 10, 0, 0)
-                )
+            grade = Grade(
+                student_id=1, 
+                course_id=2, 
+                date_posted=datetime(2024, 9, 1, 10, 0, 0)
+            )
+            with pytest.raises(IntegrityError):
                 db.session.add(grade)
                 db.session.commit()
-            except Exception as e:
-                db.session.rollback()
-                assert "grade" in str(e), "grade validation did not raise an error as expected"
 
     def test_requires_date_posted(self):
         """Requires each record to have date_posted"""
-
         with app.app_context():
             Grade.query.delete()
             db.session.commit()
 
-            try:
-                grade = Grade(
-                    student_id=1, 
-                    course_id=2, 
-                    grade=20, 
-                    date_posted=datetime(2024, 9, 1, 10, 0, 0)
-                )
+            grade = Grade(
+                student_id=1, 
+                course_id=2, 
+                grade=20, 
+            )
+            with pytest.raises(IntegrityError):
                 db.session.add(grade)
                 db.session.commit()
-            except Exception as e:
-                db.session.rollback()
-                assert "date_posted" in str(e), "date_posted did not raise an error as expected"
 
-    def test_student_id_must_be_integer(self):
-        """Requires student_id to be an integer"""
-
-        with app.app_context():
-            Grade.query.delete()
-            db.session.commit
-
-            try:
-
-                grade = Grade(
-                    student_id='one', 
-                    course_id=2, 
-                    grade=20, 
-                    date_posted=datetime(2024, 9, 1, 10, 0, 0)
-                )
-                db.session.add(grade)
-                db.session.commit()
-            except Exception as e:
-                db.session.rollback()
-                assert "student_id" in str(e), "Non-integer student_id did not raise an error as expected"
     
-    def test_course_id_must_be_integer(self):
-        """Requires course_id to be an integer"""
-
-        with app.app_context():
-            Grade.query.delete()
-            db.session.commit
-
-            try:
-
-                grade = Grade(
-                    student_id=1, 
-                    course_id="two", 
-                    grade=20, 
-                    date_posted=datetime(2024, 9, 1, 10, 0, 0)
-                )
-                db.session.add(grade)
-                db.session.commit()
-            except Exception as e:
-                db.session.rollback()
-                assert "course_id" in str(e), "Non-integer course_id did not raise an error as expected"
-
-    def test_grade_must_be_integer(self):
-        """Requires grade to be an integer"""
-
-        with app.app_context():
-            Grade.query.delete()
-            db.session.commit
-
-            try:
-                grade = Grade(
-                    student_id=1, 
-                    course_id=2, 
-                    grade='twenty', 
-                    date_posted=datetime(2024, 9, 1, 10, 0, 0)
-                )
-                db.session.add(grade)
-                db.session.commit()
-            except Exception as e:
-                db.session.rollback()
-                assert "grade" in str(e), "Non-integer grade did not raise an error as expected"
-
-    def test_valid_upload_date(self):
-        """Requires upload_date to not be in the future"""
-
-        with app.app_context():
-            Grade.query.delete()
-            db.session.commit()
-
-            future_date = datetime(2050, 9, 1, 10, 0, 0)
-            try:
-                grade = Grade(
-                    student_id=1, 
-                    course_id=2, 
-                    grade=20, 
-                    date_posted=future_date
-                )
-                db.session.add(grade)
-                db.session.commit()
-            except Exception as e:
-                db.session.rollback()
-                assert "future_date" in str(e), "future_date did not raise an error as expected"
